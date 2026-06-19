8c4ef02be42e3ec2b8607cc63c3a025107733a56
williammachadocardoso-stack <william.machado.cardoso@gmail.com>
2026-06-19 01:43:17 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+const API = "SUA_URL_DO_APPS_SCRIPT";
+
+function carregar() {
+  const id = document.getElementById("id").value;
+
+  fetch(API + "?id=" + id)
+    .then(r => r.json())
+    .then(data => {
+      document.getElementById("nome").value = data.nome;
+      document.getElementById("classe").value = data.classe;
+      document.getElementById("hp").value = data.hp;
+      document.getElementById("mana").value = data.mana;
+      document.getElementById("forca").value = data.forca;
+    });
+}
+
+function salvar() {
+  const data = {
+    id: document.getElementById("id").value,
+    nome: document.getElementById("nome").value,
+    classe: document.getElementById("classe").value,
+    hp: document.getElementById("hp").value,
+    mana: document.getElementById("mana").value,
+    forca: document.getElementById("forca").value
+  };
+
+  fetch(API, {
+    method: "POST",
+    body: JSON.stringify(data)
+  });
+
+  alert("Salvo!");
+}
